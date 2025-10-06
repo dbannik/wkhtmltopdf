eee12b87aaade228c9cfdb1ef46627d064d4bf81
Dmitry Bannik <dmitrybannik@gmail.com>
2025-10-06 20:43:34 +0300

    init

---
+++ wkhtmltoimage-install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+dpkg --add-architecture amd64 \
+    && apt update \
+    && apt install -y libc6:amd64 libxrender1:amd64 libfontconfig1:amd64 libxi6:amd64 libstdc++6:amd64 qemu-user-static --no-install-recommends \
+    && mkdir /tmp/qemu-amd64 \
+    && cp -R /usr/bin/qemu-x86_64* /usr/bin/qemu-amd64* /tmp/qemu-amd64 \
+    && rm /usr/bin/qemu-* \
+    && mv /tmp/qemu-amd64/* /usr/bin/ \
+    && rm -rf /var/lib/apt/lists/* /tmp/qemu-amd64
+
+curl -L https://github.com/h4cc/wkhtmltoimage-amd64/raw/refs/tags/0.12.4/bin/wkhtmltoimage-amd64 -o /usr/local/bin/wkhtmltoimage-amd64
+
+cat > /usr/local/bin/wkhtmltoimage <<'EOF'
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+BIN_PATH="$SCRIPT_DIR/wkhtmltoimage-amd64"
+
+if [[ "$(uname -m)" == "x86_64" ]]; then
+    "$BIN_PATH" "$@"
+else
+    qemu-amd64-static "$BIN_PATH" "$@"
+fi
+
+EOF
+
+chmod +x /usr/local/bin/wkhtmltoimage-amd64 /usr/local/bin/wkhtmltoimage
